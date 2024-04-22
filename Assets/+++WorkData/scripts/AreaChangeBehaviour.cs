006e38147a49d10f2878d1f9c512c5f7bda6446f
KarinaKre <148357884+KarinaKre@users.noreply.github.com>
2024-04-22 11:05:37 +0200

    22.04

--- Assets/+++WorkData/scripts/AreaChangeBehaviour.cs
+++ Assets/+++WorkData/scripts/AreaChangeBehaviour.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class AreaChangeBehaviour : MonoBehaviour
 {
+    public Animator animDoor;
     public Transform portPosition;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = portPosition.position;
+            StartCoroutine(InitiateTeleport(other));
         }
-       
     }
-    
+
+    IEnumerator InitiateTeleport(Collider2D other)
+    {
+        animDoor.Play("Panel_fade_in");
+        yield return new WaitForSeconds(3);
+        other.transform.position = portPosition.position;
+        yield return new WaitForSeconds(.5f);
+        animDoor.Play("Panel_fade_out");
+    }
 }
